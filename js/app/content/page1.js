4a33a18601e703493b628e1aacfe83e5c88fcfb7
Vitek Janota <vitek.janota@xyleme.com>
2020-09-21 20:40:09 +0200

    fixing internal link to non existing page

--- js/app/content/page1.js
+++ js/app/content/page1.js
@@ -2,7 +2,7 @@ var Page1 = new ContentRenderer(
 	pageContentRef,
 	"<div>\n\
 		<h2>%%title%%</h2>\n\
-		<p>This text contains %%text1%% to <a href='%%url1%%' class='nternalLink'>%%link1%%</a> and to <a href='%%url2%%' class='internalLink'>%%link2%%</a>. %%text2%%</p>\n\
+		<p>This text contains %%text1%% to <a href='%%url1%%' class='internalLink'>%%link1%%</a> and to <a href='%%url2%%' class='internalLink'>%%link2%%</a>. %%text2%%</p>\n\
 	</div>",
 	{
 		content: {
@@ -11,7 +11,7 @@ var Page1 = new ContentRenderer(
 			link1: "homepage",
 			url1: appIndexPath,
 			link2: "nonexisting page",
-			url2: appIndexPath + "page4/",
+			url2: appIndexPath + "page5/",
 			text2: "Both links were altered after the content rendering to prevent document relaod."
 		}
 	});

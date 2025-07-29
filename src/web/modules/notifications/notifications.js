dafd219b2a2201d721fee2e13ab7714eacc77f64
Vit Janota <vitek@Vit--MacBook-Pro.local>
2025-07-29 17:12:49 +0200

    adding configuration options, fixing this context, adding configurable auto hide

--- src/web/modules/notifications/notifications.js
+++ src/web/modules/notifications/notifications.js
@@ -1,53 +1,67 @@
 // ======================== notifications ========================
+// version 0.1
 
 function Notifications() {
-    this.hider;
     this.notificationsList = {};
     this.contentTemplates = {
-        default: `<span>%%message%%</span>
-            <span data-process="link">
-                <span data-type="text">%%content%%</span>
+        default: `<h1>%%title%%</h1>
+            <div data-process="body">
+                <span>%%content%%</span>
                 <a href="%%src%%" data-type="link">%%content%%</a>
-            </span>`,
-        warning: `<h1>%%message%%</h1>`
+            </div>`,
+        warning: `<h1>Warning: %%message%%</h1>`
     };
+    this.setup = {
+        units: 'px',
+        topOffset: 30,
+        areaWidth: 300,
+        displayDuration: 2000,
+        autoHide: false
+    }
 
     // emit new notification
-    this.emit = function(msg,type) {
+    this.emit = (msg,type) => {
         const nid = crypto.randomUUID();
-        this.notificationsList[nid] = {block: this.createNotifier()};
+        this.notificationsList[nid] = {block: this.createNotifier(nid)};
         this.showMessage(nid,msg,type);
-        this.hider = setTimeout(this.hideMessage,3000,nid,this);
     }
 
     // show notification message
-    this.showMessage = function(id,msg,type) {
+    this.showMessage =(id,msg,type) => {
         let block = this.notificationsList[id].block;
         this.addContent(block,msg,type)
-        block.style.marginTop = (window.pageYOffset + 100) + 'px';
+        block.style.marginTop = (window.pageYOffset + this.setup.topOffset) + this.setup.units;
         block.style.display = 'block';
-        this.slide(this,block,'right',-300,0,3);
+        const callback = this.setup.autoHide ? () => {setTimeout(this.hideMessage,this.setup.displayDuration,id)} : null;
+        this.slideIt(block,'right',(-1 * this.setup.areaWidth - 20),0,3,callback);
     }
 
      // hide notification message
-    this.hideMessage = function(id,that) {
-        that.slide(that,that.notificationsList[id].block,'right',0,-400,-3,function(){that.removeNotifier(id)});
+    this.hideMessage = (id) => {
+        const callback = () => {this.removeNotifier(id)};
+        this.slideIt(this.notificationsList[id].block,'right',0,(-1 * this.setup.areaWidth - 120),-3,callback);
     }
 
     // create notification element
-    this.createNotifier = function() {
+    this.createNotifier = (id) => {
         // read position of the last notification and place new one below it
         const lastArea = [...document.querySelectorAll('.notificationArea')].pop();
-        const newPos = lastArea ? (Number(lastArea.style.top.replace('px','')) + 100) : 0;
+        // !!!!!!!!!
+        const newPos = lastArea ? (Number(lastArea.style.top.replace(this.setup.units,'')) + 100) : 0;
         const notificationBlock = document.createElement('div');
         notificationBlock.setAttribute('class',`notificationArea`);
-        notificationBlock.setAttribute('style',`top: ${newPos}px`);
+        notificationBlock.style.top = `${newPos}${this.setup.units}`;
+        notificationBlock.style.width = `${this.setup.areaWidth}${this.setup.units}`;
+        if (!this.setup.autoHide) {
+            notificationBlock.addEventListener('click',() => {this.hideMessage(id)});
+            notificationBlock.style.cursor = 'pointer';
+        }
         document.getElementsByTagName('body')[0].appendChild(notificationBlock);
         return notificationBlock;
     }
 
     // add content to notification element
-    this.addContent = function(block,msg,type) {
+    this.addContent = (block,msg,type) => {
         if (typeof msg == 'string') { // content is a string, add it as is
             block.innerHTML = msg;
         } else if ((msg instanceof Array) || (msg instanceof Object)) {  // content is structured, use template to display it
@@ -58,18 +72,18 @@ function Notifications() {
     }
 
     // remove notification element
-    this.removeNotifier = function(id) {
+    this.removeNotifier = (id) => {
         this.notificationsList[id].block.remove();
         delete this.notificationsList[id];
     }
 
     // slide element
-    this.slide = function(that,slider,position,progress,end,step,callback) {
-        slider.style[position] = progress + "px"
+    this.slideIt = (slider,position,progress,end,step,callback) => {
+        slider.style[position] = `${progress}${this.setup.units}`;
         if (Math.abs(end - progress) >= Math.abs(step)) {
             progress += step;
-            setTimeout(function(){
-                that.slide(that,slider,position,progress,end,step,callback);
+            setTimeout(() => {
+                this.slideIt(slider,position,progress,end,step,callback);
             },4);
         } else {
             if (callback) callback();
